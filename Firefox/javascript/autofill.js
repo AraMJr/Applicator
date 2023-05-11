49a1531337ba992dad28b51835557970634c009e
Ara Megerdichian <72160824+AraMJr@users.noreply.github.com>
2023-05-11 01:53:20 -0600

    refined styling

--- Firefox/javascript/autofill.js
+++ Firefox/javascript/autofill.js
@@ -1,33 +1,33 @@
 console.log("Applicator extension loaded");
 
 // Define the interface
-const applicate = document.getElementById("applicate");
-const firstNameInput = document.getElementById("first-name");
-const phoneNumberInput = document.getElementById("phone-number");
-const emailInput = document.getElementById("email");
+document.addEventListener('DOMContentLoaded', function() {
+    var inputs = document.querySelectorAll('input[data-field]');
 
-// Load user data
-browser.storage.local.get(["first-name", "phone-number", "email"]).then((result) => {
-  firstNameInput.value = result.firstName || "";
-  phoneNumberInput.value = result.phoneNumber || "";
-  emailInput.value = result.email || "";
-});
-
-// Store user data on form submission
-const form = document.querySelector("form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const firstName = firstNameInput.value;
-  const phoneNumber = phoneNumberInput.value;
-  const email = emailInput.value;
-  browser.storage.local.set({ firstName, phoneNumber, email });
-});
-
-// Auto-populate form fields
-applicate.addEventListener("click", () => {
-    browser.tabs.executeScript({ file: "content-script.js" }).then(() => {
-      console.log("Form auto-populated");
-    }).catch((error) => {
-      console.error(error);
+    // Load the last entered values from storage and populate the input fields
+    browser.storage.local.get(null, function(result) {
+        for (var i = 0; i < inputs.length; i++) {
+            var field = inputs[i].dataset.field;
+            if (result[field]) {
+                inputs[i].value = result[field];
+            }
+        }
     });
+
+    // Save the values when the user types or changes the input fields
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('input', function(event) {
+            var field = event.target.dataset.field;
+            var value = event.target.value;
+
+            // Store the last entered value in storage
+            var data = {};
+            data[field] = value;
+            browser.storage.local.set(data);
+        });
+    }
+
+    
+
 });
+
